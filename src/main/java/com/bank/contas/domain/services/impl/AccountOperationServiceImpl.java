1ed0ee1ae26ed30f96b8314f8945d8d89fcc4cd9
Joel Maciel <soldado25461@gmail.com>
2023-03-28 16:51:09 -0300

    style: removendo codigo comentado

--- src/main/java/com/bank/contas/domain/services/impl/AccountOperationServiceImpl.java
+++ src/main/java/com/bank/contas/domain/services/impl/AccountOperationServiceImpl.java
@@ -1,13 +1,10 @@
 package com.bank.contas.domain.services.impl;
 
-import com.bank.contas.domain.exceptions.AmountNotFoundException;
-import com.bank.contas.domain.exceptions.BusinessException;
 import com.bank.contas.domain.repositories.AccountRepository;
 import com.bank.contas.domain.services.AccountOperationService;
 import com.bank.contas.domain.services.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -36,14 +33,9 @@ public class AccountOperationServiceImpl implements AccountOperationService {
 
     @Override
     public void withdraw(UUID accountId, BigDecimal amount) {
-//        try {
             var account = accountService.findAccountId(accountId);
             account.withdraw(amount);
             accountRepository.save(account);
-//        } catch (NullPointerException e){
-//            throw new AmountNotFoundException("The value must be greater than zero");
-//        }
-
     }
 
     @Override
