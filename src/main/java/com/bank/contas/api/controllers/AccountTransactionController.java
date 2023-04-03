f522ac6a1854a5d49e9bff5882035225eb68bcd6
Joel Maciel <soldado25461@gmail.com>
2023-04-02 22:32:29 -0300

    feat: recebendo token vindo do microservice User para controler do acesso dos métodos do controller

--- src/main/java/com/bank/contas/api/controllers/AccountTransactionController.java
+++ src/main/java/com/bank/contas/api/controllers/AccountTransactionController.java
@@ -2,6 +2,7 @@ package com.bank.contas.api.controllers;
 
 import com.bank.contas.domain.services.AccountOperationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.util.annotation.Nullable;
 
@@ -16,20 +17,25 @@ public class AccountTransactionController {
 
     private final AccountOperationService accountOperationService;
 
+    @PreAuthorize("hasAnyRole('CUSTOMER')")
     @GetMapping("{accountId}/balance")
     public BigDecimal getBalance(@PathVariable UUID accountId) {
         return accountOperationService.consultBalance(accountId);
     }
+
+    @PreAuthorize("hasAnyRole('CUSTOMER')")
     @PutMapping("/{accountId}/deposit")
     public void depositAccount(@PathVariable UUID accountId, @Nullable @RequestParam BigDecimal amount) {
         accountOperationService.deposit(accountId, amount);
     }
 
+    @PreAuthorize("hasAnyRole('CUSTOMER')")
     @PutMapping("/{accountId}/withdraw")
     public void withdrawAccount(@PathVariable UUID accountId, @Nullable @RequestParam BigDecimal amount) {
         accountOperationService.withdraw(accountId, amount);
     }
 
+    @PreAuthorize("hasAnyRole('CUSTOMER')")
     @PutMapping("/{accountIdOrigin}/{accountIdDestiny}/transfer")
     public void transferAccount(@PathVariable UUID accountIdOrigin,  @PathVariable UUID accountIdDestiny,
                                                                      @Nullable @RequestParam BigDecimal amount) {
--- src/main/java/com/bank/contas/api/controllers/AgencyController.java
+++ src/main/java/com/bank/contas/api/controllers/AgencyController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,28 +23,33 @@ public class AgencyController {
 
     private final AgencyService agencyService;
 
+    @PreAuthorize("hasAnyRole('EMPLOYEE')")
     @GetMapping
     public Page<AgencyResponseDTO> getAllAgencies(SpecificationTemplate.AgencySpec spec,
          @PageableDefault(page = 0, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
         return agencyService.findAll(spec, pageable);
     }
 
+    @PreAuthorize("hasAnyRole('EMPLOYEE')")
     @GetMapping("/{agencyId}")
     public AgencyResponseDTO getOneAgency(@PathVariable UUID agencyId) {
         return agencyService.findByAgency(agencyId);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public AgencyResponseDTO saveAgency(@RequestBody @Valid AgencyDTO agencyDTO) {
         return agencyService.save(agencyDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping("/{agencyId}")
     public AgencyResponseDTO updateAgency(@PathVariable UUID agencyId, @RequestBody @Valid AgencyDTO agencyDTO) {
         return agencyService.updateAgency(agencyId, agencyDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/{agencyId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAgency(@PathVariable UUID agencyId) {
